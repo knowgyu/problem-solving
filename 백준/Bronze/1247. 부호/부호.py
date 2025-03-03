3c88b5fb5015e62bbae4298da74544043bc4bfc2
knowgyu <gyuheon.noh@gmail.com>
2025-03-03 20:37:36 +0900

    [Bronze III] Title: 부호, Time: 168 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+for _ in range(3):
+    n = int(input())
+    total = 0
+    for _ in range(n):
+        total += int(input())
+    if total > 0:
+        print("+")
+    elif total < 0:
+        print("-")
+    else:
+        print("0")
