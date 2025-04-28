517f36fa08cab31fca0ff59b45fae7772885a514
knowgyu <gyuheon.noh@gmail.com>
2025-04-29 08:50:14 +0900

    [Bronze IV] Title: Quick Estimates, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N; cin >> N;
+    string s;
+    while(N--)
+    {
+        cin >> s;
+        cout << s.size() << '\n';
+    }
+    return 0;
+}
